70bae78994e2055b28277eec184d47b764fedd2a
Ashwini <akamzing412@gmail.com>
2021-04-09 01:28:35 +0530

    .

--- Pointer/rpaper_dim.c
+++ Pointer/rpaper_dim.c
@@ -1,24 +1,10 @@
-#include <stdio.h>
-int print_paper_dimension(int a, int b);
-void main()
-
+int rec(int a,int b)
 {
-    int a = 1189, b = 841;
-    print_paper_dimension(a, b);
-    printf("%d mm * %d mm\n", a, b);
-}
-
-int print_paper_dimension(int a, int b)
-{
-    int i = 8;
-    if (i > 0 && i <= 8)
-        return 0;
-    else
-        return print_paper_dimension(a, b);
-
-    if (a > b)
-        a = a / 2;
-    else
-        b = b / 2;
-    i--;
+int i=0;
+while(i<8)
+{ printf("%d mm* %d mm\n",a,b);
+if(a>b)
+return rec(a/2,b);
+else
+return rec(a,b/2);
 }
